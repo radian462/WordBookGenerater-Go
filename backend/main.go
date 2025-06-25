1ca572f9ce91f3a059fdc4598a401bbfff7a9e1d
radian462 <102207047+radian462@users.noreply.github.com>
2025-06-25 03:46:39 +0000

    Wordbook Goのロゴをいい感じに

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"log"
+	"net/http"
 )
 
 func main() {
-    fs := http.FileServer(http.Dir("frontend/static"))
-    http.Handle("/", fs)
+	fs := http.FileServer(http.Dir("frontend/static"))
+	http.Handle("/", fs)
 
-    log.Println("Server started at :8080")
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	log.Println("Server started at :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
